c12889207c7ae8273ef8ad21c67402644d4c870d
jun-ho-Kim <wnsgh5049@naver.com>
2022-02-14 08:20:43 +0900

    Fetching Data
    - themoviedb.org 사이트에서 API_KEY를 이용해 데이터 가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import NavBar from "../components/NavBar"
 
-export default function hi() {
-    const [counter, setCounter] = useState(0)
+export default function Home() {
+    const API_KEY = process.env.API_KEY
+
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+        (async () => {
+            const { results } = await (await fetch(
+                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+            )
+            ).json()
+            setMovies(results)
+        })()
+    }, [])
     return (
         <div>
             <Header title="Home" />
-            <h1>hi {counter}</h1>
-            <button onClick={() => setCounter((prv) => prv + 1)}>+</button>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+
+                </div>
+            ))}
+            <h1>hi</h1>
+
             <style jsx global>{`
                 a {
                     color: white
