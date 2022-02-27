a5c2e71932662c8a0deaa3fc20634d2ff8c9ccfd
jun-ho-Kim <wnsgh5049@naver.com>
2022-02-27 16:04:03 +0900

    Movie Detail
    - router.push를 사용하여 URL를 숨기는 방법
    - router.push에 query에 데이터 넣어 보내주어 이동하는 URL에 넣어서 보내주기

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,72 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import Header from "../components/Header"
 
 export default function Home({ results }) {
+    const router = useRouter()
+
+    const onClick = (id, title) => {
+        router.push(
+            {
+                pathname: `/movies/${id}`,
+                query: {
+                    title,
+                }
+            },
+            `/movies/${id}`
+        )
+    }
     return (
         <div>
             <Header title="Home" />
             {results?.map((movie) => (
-                <div key={movie.id}>
-                    <h4>{movie.original_title}</h4>
+                <div
+                    onClick={() => onClick(movie.id, movie.original_title)}
+                    className='movie'
+                    key={movie.id}
+                >
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                    <h4>
+                        <Link
+                            href={{
+                                pathname: `/movies/${movie.id}`,
+                                query: {
+                                    title: movie.original_title,
+                                },
+                            }}
+                            as={`/movies/${movie.id}`}
+                        >
+                            <a>{movie.original_title}</a>
+                        </Link>
+                    </h4>
                 </div>
-            ))}
-            <h1>hi</h1>
+            ))
+            }
+            <style jsx>{`
+                .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 20px;
+                gap: 20px;
+                }
+                .movie {
+                cursor: pointer;
+                }
+                .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+                }
+                .movie:hover img {
+                transform: scale(1.05) translateY(-10px);
+                }
+                .movie h4 {
+                font-size: 18px;
+                text-align: center;
+                }
+      `}</style>
         </div>
     )
 }
